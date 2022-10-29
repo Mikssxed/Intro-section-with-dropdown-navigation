6ea9a350cf5aa145fad287a01e7eaea2984033cb
Mikssxed <112573426+Mikssxed@users.noreply.github.com>
2022-10-29 23:00:51 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const list = document.querySelectorAll('.list2');
+const show = document.querySelector('#menu');
+const header2 = document.querySelector('#header2');
+const dark = document.querySelector('#darker');
+const hide = document.querySelector('#close')
+
+list.forEach(function(item){
+    item.addEventListener('click', function(){
+        item.parentElement.classList.toggle('show')
+    })
+})
+
+show.addEventListener('click', function(){
+    header2.classList.add('active')
+    dark.classList.add('active')
+})
+
+hide.addEventListener('click', function(){
+    header2.classList.remove('active')
+    dark.classList.remove('active')
+})
